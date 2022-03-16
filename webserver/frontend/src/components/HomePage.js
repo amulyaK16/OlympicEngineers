fde1d8a4846651cd92acce707157e4de7d8ba27b
Haydn Brown <haydnpbrown1@gmail.com>
2022-03-16 00:14:39 -0400

    further frontend work

--- webserver/frontend/src/components/HomePage.js
+++ webserver/frontend/src/components/HomePage.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import LoginPage from './LoginPage';
 import SignedInPage from './SignedInPage';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import Button from "@material-ui/core/Button"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
+import FormHelperText from "@material-ui/core/FormHelperText"
+import FormControl from "@material-ui/core/FormControl"
+import Radio from "@material-ui/core/Radio"
+import RadioGroup from "@material-ui/core/RadioGroup"
+import { FormControlLabel } from '@material-ui/core/FormControlLabel';
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -13,13 +22,11 @@ export default class HomePage extends Component {
         <Router>
             <Switch>
                 <Route exact path='/'>
-                    <p>This is the home page</p>
+                    <p>This is the home/routing page</p>
                 </Route>
                 <Route path='/login' component={LoginPage}>
-
                 </Route>
                 <Route path='/signedin' component={SignedInPage}>
-
                 </Route>
             </Switch>
         </Router>)
--- webserver/frontend/src/components/SignedInPage.js
+++ webserver/frontend/src/components/SignedInPage.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { Link} from "react-router-dom";
+import { FormControlLabel } from '@material-ui/core/FormControlLabel';
+import { TextField, Button, Grid, Typography, FormHelperText, FormControl, Radio, RadioGroup} from "@material-ui/core";
+import { Line } from 'react-chartjs-2';
 
 export default class SignedInPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            datafirst: {
+                labels: ["1","2","3","4","5","6","7","8","9","10"],
+                datasets: {
+                    label: 'set 1',
+                    data: [14,15,13,17,19,21,22,20,18,16],
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                }
+            },
+            graphoptions: {
+                maintainAspectRatio: true,
+                title: {
+                  display: true,
+                  text: "Largest cities of Massachusetts"
+                },
+                legend: { display: true, position: "bottom" }
+              }
+        }
     }
 
     render() {
-        return <p> The user page.</p>
+        return (
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <p>The default page</p> 
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Line options={this.state.graphoptions} data={this.state.datafirst} />   
+            </Grid>
+        </Grid>);
     }
 }
