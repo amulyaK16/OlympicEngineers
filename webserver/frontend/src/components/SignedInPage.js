86b03239ae7cd2c993b83fed7791596e4d393f90
Haydn Brown <haydnpbrown1@gmail.com>
2022-03-16 23:19:11 -0400

    chart running

--- webserver/frontend/src/components/SignedInPage.js
+++ webserver/frontend/src/components/SignedInPage.js
@@ -1,42 +1,90 @@
-import React, { Component } from 'react';
-import { Link} from "react-router-dom";
-import { FormControlLabel } from '@material-ui/core/FormControlLabel';
-import { TextField, Button, Grid, Typography, FormHelperText, FormControl, Radio, RadioGroup} from "@material-ui/core";
-import { Line } from 'react-chartjs-2';
+import React, { Component, useState } from "react";
+import { Link } from "react-router-dom";
+import { FormControlLabel } from "@material-ui/core/FormControlLabel";
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  FormHelperText,
+  FormControl,
+  Radio,
+  RadioGroup,
+  Card,
+  CardHeader,
+  CardContent,
+  IconButton
+} from "@material-ui/core";
+import { Line } from "react-chartjs-2";
+import "chartjs-plugin-streaming";
+import moment from "moment";
+import { Bar } from "react-chartjs-2";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-export default class SignedInPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            datafirst: {
-                labels: ["1","2","3","4","5","6","7","8","9","10"],
-                datasets: {
-                    label: 'set 1',
-                    data: [14,15,13,17,19,21,22,20,18,16],
-                    borderColor: 'rgb(255, 99, 132)',
-                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                }
-            },
-            graphoptions: {
-                maintainAspectRatio: true,
-                title: {
-                  display: true,
-                  text: "Largest cities of Massachusetts"
+function Chart1(){
+    const data = {
+        datasets: [
+          {
+            label: "Dataset 1",
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            lineTension: 0,
+            borderDash: [8, 4],
+            data: []
+          }
+        ]
+      };
+    
+      const options = {
+        scales: {
+          xAxes: [
+            {
+              type: "realtime",
+              realtime: {
+                onRefresh: function() {
+                  data.datasets[0].data.push({
+                    x: Date.now(),
+                    y: Math.random() * 100
+                  });
                 },
-                legend: { display: true, position: "bottom" }
+                delay: 2000
               }
+            }
+          ]
         }
-    }
+      };
+    
+      return (
+      <div>
+        <Line data={data} options={options} />
+      </div>);
+}
 
-    render() {
-        return (
-        <Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-                <p>The default page</p> 
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Line options={this.state.graphoptions} data={this.state.datafirst} />   
-            </Grid>
-        </Grid>);
-    }
+
+function Greeting(props) {
+  return <p>{props.value}, Haydn...</p>;
+}
+
+export default function SignedInWrapper() {
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Greeting value={"Hello"} />
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Card elevation={10}>
+          <CardHeader
+            title={
+              <Typography variant="h5">
+                Graph 1
+              </Typography>
+            }
+          />
+          <CardContent style={{ backgroundColor: "#fff" }}>
+            <Chart1 />
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
