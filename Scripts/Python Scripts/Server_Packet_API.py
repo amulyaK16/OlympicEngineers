e35757d1d80b218e81b2e71da9b9633dcd4514f2
chasefridgen <55394210+chasefridgen@users.noreply.github.com>
2022-03-20 15:00:45 -0400

    final implementation of server_API

--- Scripts/Python Scripts/Server_Packet_API.py
+++ Scripts/Python Scripts/Server_Packet_API.py
@@ -11,10 +11,11 @@ def connect_to_HC05_COM_port():
             return p.device
     raise IOError("Could not find STM device. Make sure it's plugged in?")
 
+
 def create_serial():
     c_port = connect_to_HC05_COM_port()
     port = serial.Serial(c_port, baudrate=9600, bytesize=8, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
-                         timeout=25)
+                         timeout=8)
     return port
 
 
@@ -28,14 +29,18 @@ def getAck(port, n):
     else:
         return False
 
+
 def send_Ping(port):
     return getAck(port, '1')
+
 
 def send_start_cmd(port):
     return getAck(port, '2')
 
+
 def send_stop_cmd(port):
     return getAck(port, '3')
+
 
 def receive_data(port):
     rcv = port.read(190)
@@ -53,8 +58,14 @@ param:
 
 return: unpacked packet list
 '''
+
+
 def bytes_to_packet(rcv_bytes):
-    return struct.unpack('IHHB32H32HfffffffB21c', rcv_bytes)
+    p1 = struct.unpack('IHHB', rcv_bytes[0:9])
+    p2 = struct.unpack_from('31H31H', rcv_bytes, 9)
+    p3 = struct.unpack_from('fffffffB24c', rcv_bytes, 137)
+    return p1 + p2 + p3
+
 
 '''
 packet_to_dict
@@ -67,24 +78,29 @@ param:
 
 return: python dict corresponding to packet_t struct
 '''
+
+
 def packet_to_dict(packet):
     pack_dict = {}
 
-    pack_dict["START"]    = packet[0]
+    pack_dict["START"] = packet[0]
     pack_dict["PKT_SIZE"] = packet[1]
-    pack_dict["PKT_NUM"]  = packet[2]
-    pack_dict["STATE"]    = packet[3]
-    pack_dict["ECG"]      = packet[4:37]
-    pack_dict["EMG"]      = packet[37:70]
-    pack_dict["GYRO_X"]   = packet[70]
-    pack_dict["GYRO_Y"]   = packet[71]
-    pack_dict["GYRO_Z"]   = packet[72]
-    pack_dict["ACCEL_X"]  = packet[73]
-    pack_dict["ACCEL_Y"]  = packet[74]
-    pack_dict["ACCEL_Z"]  = packet[75]
-    pack_dict["TIME"]     = b''.join(packet[-21:-4])
+    pack_dict["PKT_NUM"] = packet[2]
+    pack_dict["STATE"] = packet[3]
+    pack_dict["ECG"] = packet[4:35]
+    pack_dict["EMG"] = packet[35:66]
+    pack_dict["FORCE"] = packet[66]
+    pack_dict["GYRO_X"] = packet[67]
+    pack_dict["GYRO_Y"] = packet[68]
+    pack_dict["GYRO_Z"] = packet[69]
+    pack_dict["ACCEL_X"] = packet[70]
+    pack_dict["ACCEL_Y"] = packet[71]
+    pack_dict["ACCEL_Z"] = packet[72]
+    pack_dict["PL_SIZE"] = packet[73]
+    pack_dict["TIME"] = str(b''.join(packet[74:]), "utf-8")
 
     return pack_dict
+
 
 '''
 dict_to_json
@@ -97,6 +113,8 @@ param:
 
 return: json of received packet
 '''
+
+
 def dict_to_json(pack_dict):
     return json.dumps(pack_dict)
 
@@ -117,15 +135,40 @@ if __name__ == '__main__':
     if send_start_cmd(port):
         print('packet collection started')
 
-    while not isnum:
-        BtP = bytes_to_packet(receive_data(port))
-        PtD = packet_to_dict(BtP)
+    with open('json_data.json', 'w') as outfile:
 
-        print(PtD["PKT_NUM"])
+        while not isnum:
+            test = receive_data(port)
+            print(test.hex(" "))
+            BtP = bytes_to_packet(test)
+            pack_dict = packet_to_dict(BtP)
+            js = dict_to_json(pack_dict)
 
-        count += 1
-        if count == 20:
-            isnum = True
+            json.dump(js, outfile)
+
+            print(js)
+
+            print(pack_dict["START"])
+            print(pack_dict["PKT_SIZE"])
+            print(pack_dict["PKT_NUM"])
+            print(pack_dict["STATE"])
+            print(pack_dict["ECG"])
+            print(pack_dict["EMG"])
+            print(pack_dict["FORCE"])
+            print(pack_dict["GYRO_X"])
+            print(pack_dict["GYRO_Y"])
+            print(pack_dict["GYRO_Z"])
+            print(pack_dict["ACCEL_X"])
+            print(pack_dict["ACCEL_Y"])
+            print(pack_dict["ACCEL_Z"])
+            print(pack_dict["PL_SIZE"])
+            print(pack_dict["TIME"])
+            print("")
+            print("")
+
+            count += 1
+            if count == 1000:
+                isnum = True
 
 
 
